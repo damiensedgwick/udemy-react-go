f8a969462d6004f4ca0448d1e20174e96aa065d8
Damien Sedgwick <damiensedgwick@icloud.com>
2021-12-16 19:55:12 +0000

    Fixed packages

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -41,7 +41,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production")
-	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://tcs@localhost/go_movies?sslmode=disable", "Postgres connection string")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://root:password@localhost/go_movies?sslmode=disable", "Postgres connection string")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
