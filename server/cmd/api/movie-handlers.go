1903d0aa26359a756c9c122174fdafe83cf0edfb
Damien Sedgwick <damiensedgwick@icloud.com>
2021-12-16 17:35:41 +0000

    Added ability to get one movie from the DB

--- server/cmd/api/movie-handlers.go
+++ server/cmd/api/movie-handlers.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"errors"
-	"net/http"
-	"server/models"
-	"strconv"
-	"time"
-
 	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
 )
 
 func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
@@ -22,18 +19,7 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 
 	app.logger.Println("id is", id)
 
-	movie := models.Movie{
-		ID:          id,
-		Title:       "Beethoven",
-		Description: "A slobbering St. Bernard becomes the center of attention for a loving family, but must contend with a dog-napping veterinarian and his henchmen.",
-		Year:        1992,
-		ReleaseDate: time.Date(1992, 11, 04, 01, 0, 0, 0, time.Local),
-		RunTime:     87,
-		Rating:      6,
-		MPAARating:  "U",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
+	movie, err := app.models.DB.Get(id)
 
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
 }
