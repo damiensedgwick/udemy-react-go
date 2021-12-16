118b292c195cbe86173204b29c2f919b33e21da4
Damien Sedgwick <damiensedgwick@icloud.com>
2021-12-16 20:48:15 +0000

    renaming things

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -39,7 +39,7 @@ type application struct {
 func main() {
 	var cfg config
 
-	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.IntVar(&cfg.port, "port", 8080, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production")
 	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://root:password@localhost/go_movies", "Postgres connection string")
 	flag.Parse()
