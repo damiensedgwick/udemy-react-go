7c9b41cb1eadc4868404d5408ac7509cac186666
Damien Sedgwick <damiensedgwick@icloud.com>
2021-12-18 16:57:12 +0000

    Frontend can query backend

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import { useParams } from "react-router-dom";
-
-import { getMovie } from "../utils/movies";
 
 export const Movie = () => {
   const params = useParams();
-  const movie = getMovie(parseInt(params.movieId, 10));
+  const movieId = parseInt(params.movieId, 10);
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(`http://localhost:8080/v1/movie/${movieId}`)
+      .then((response) => {
+
+        if (response.status !== 200) {
+          let err = Error
+          err.message = "Invalid response code: " + response.status
+          setError({error: err})
+        }
+
+        return response.json()
+      })
+      .then((json) => {
+          setMovie(json.movie)
+          setIsLoading(false)
+        },
+        (error) => {
+          setIsLoading(false)
+          setError(error)
+        }
+      )
+  }, [movieId]);
+
+  if (isLoading) return (
+    <div>
+      <h2 className="text-xl mb-4">Loading...</h2>
+    </div>
+  )
+
+  if (error) return (
+    <div>
+      <h2 className="text-xl mb-4">{`Error: ${error.message}`}</h2>
+    </div>
+  )
+
 
   return (
     <div className="border border-black py-4 px-6 space-y-4">
--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -1,10 +1,45 @@
-import React, { useState } from "react";
-import { NavLink, Outlet } from "react-router-dom";
-
-import { initialMovies } from "../utils/movies";
+import React, {useState} from "react";
+import {NavLink, Outlet} from "react-router-dom";
 
 export const Movies = () => {
-  const [movies, setMovies] = useState(initialMovies);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('http://localhost:8080/v1/movies')
+      .then((response) => {
+
+        if (response.status !== 200) {
+          let err = Error
+          err.message = "Invalid response code: " + response.status
+          setError({error: err})
+        }
+
+        return response.json()
+      })
+      .then((json) => {
+        setMovies(json.movies)
+        setIsLoading(false)
+      },
+        (error) => {
+          setIsLoading(false)
+          setError(error)
+        }
+      )
+  }, []);
+
+  if (isLoading) return (
+    <div>
+      <h2 className="text-xl mb-4">Loading...</h2>
+    </div>
+  )
+
+  if (error) return (
+    <div>
+      <h2 className="text-xl mb-4">{`Error: ${error.message}`}</h2>
+    </div>
+  )
 
   return (
     <div>
@@ -18,7 +53,7 @@ export const Movies = () => {
         ))}
       </ul>
 
-      <Outlet />
+      <Outlet/>
     </div>
   );
 };
